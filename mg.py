33937b21d8c476883eb854d4b1bc556ddae166d0
Ageless2016 <mobile_luocheng@163.com>
2019-10-23 07:35:50 +0800

    # 添加header list

--- mg.py
+++ mg.py
@@ -10,9 +10,11 @@ def run(folder_path,template_path):
     tmp_wb = xw.Book(template_path)
     tmp_sheets = tmp_wb.sheets
 
+
     #获取配置sheet表
     try:
         tmp_config_sht = tmp_sheets['CONFIG']
+        tmp_data_sht = tmp_sheets[0]
     except:
         print("No 'CONFIG' sheet found!")
         tmp_wb.close()
@@ -32,7 +34,28 @@ def run(folder_path,template_path):
         dict_config['start_column']=r[4]
         user_config.append(dict_config)
 
+    end_column = tmp_data_sht.cells(1, 16384).end('left').column
 
+
+
+    #获取EXCEL数据表中第一行解析表头，存到数组head_list
+    header_list = tmp_data_sht.range(tmp_data_sht.cells(1,1),tmp_data_sht.cells(1,end_column)).value
+
+    header_pk = []
+
+    #查找是否存在pk字段，如果不存在，退出程序，提示找不到pk列，如果存在，把pk字段存成一个列表
+
+    # for  header in header_list:
+    #
+    #     if not '_' in header:
+    #
+    #         continue
+    #
+    #     if header.split('_')[0].upper == 'PK':
+    #
+    #         header_pk.append(header)
+    #
+    #
 
 
 
