032686465d1bca9d8b7ed3b3653d1f0567a77c47
Ageless2016 <mobile_luocheng@163.com>
2019-12-04 17:44:08 +0800

    # 完善EventRecord对象的hash比较

--- cprevents.py
+++ cprevents.py
@@ -28,6 +28,8 @@ def run():
 
     records_2018 =  extract_2018_records(fdpath_2018)
     records_2019 = extract_2019_records(fdpath_2019)
+
+    # print(records_2018)
     set_records_2018 = set(records_2018)
     set_records_2019 = set(records_2019)
 
@@ -42,6 +44,11 @@ def run():
 
     print('records 2019' + '='*100)
     print(records_2019_list_size,'vs',records_2019_set_size)
+
+    print('records 2019 - records 2018' + '='*100)
+    set_records_19new = set_records_2018 - set_records_2019
+    print(len(set_records_19new))
+    print(list(set_records_19new))
 
 
     print('done!')
@@ -97,12 +104,9 @@ def extract_2018_records(fdpath):
                                 moattempttime = block_event_content[j][3]
                                 if testpoint_name in str(mofilename) and testscene in str(mofilename):
                                     abnormaltype = '未接通'
-                                    abnormal_event = EventRecord.AbnormalDetail(mofilename, moattempttime)
-                                    blocked_detail_list.append(abnormal_event)
-                                    if len(blocked_detail_list) >= blockedcount:break
-                            event_record = EventRecord(city,testpoint_name, testscene, servicetype, totalcount, coveredcount,
-                                                       abnormaltype, blocked_detail_list)
-                            record_list.append(event_record)
+                                    event_record = EventRecord(city,testpoint_name, testscene, servicetype, totalcount, coveredcount,
+                                                       abnormaltype,moattempttime,mofilename)
+                                    record_list.append(event_record)
 
                         # 查找电信掉话详情表找到主叫文件名、主叫起呼时间
                         if not droppedcount is None and droppedcount > 0:
@@ -117,12 +121,9 @@ def extract_2018_records(fdpath):
                                 moattempttime = drop_event_content[k][3]
                                 if testpoint_name in str(mofilename) and testscene in str(mofilename):
                                     abnormaltype = '掉话'
-                                    abnormal_event = EventRecord.AbnormalDetail(mofilename, moattempttime)
-                                    dropped_detail_list.append(abnormal_event)
-                                    if len(dropped_detail_list) >= droppedcount:break
-                            event_record = EventRecord(city,testpoint_name, testscene, servicetype, totalcount, coveredcount,
-                                                       abnormaltype, dropped_detail_list)
-                            record_list.append(event_record)
+                                    event_record = EventRecord(city,testpoint_name, testscene, servicetype, totalcount, coveredcount,
+                                                       abnormaltype,moattempttime,mofilename)
+                                    record_list.append(event_record)
 
                 elif sht.name == sht_volte_name:
                     servicetype = 'volte'
@@ -150,12 +151,9 @@ def extract_2018_records(fdpath):
                                 moattempttime = block_event_content[j][6]
                                 if testpoint_name in str(mofilename) and testscene in str(mofilename):
                                     abnormaltype = '未接通'
-                                    abnormal_event = EventRecord.AbnormalDetail(mofilename, moattempttime)
-                                    blocked_detail_list.append(abnormal_event)
-                                    if len(blocked_detail_list) >= blockedcount:break
-                            event_record = EventRecord(city,testpoint_name, testscene, servicetype, totalcount, coveredcount,
-                                                       abnormaltype, blocked_detail_list)
-                            record_list.append(event_record)
+                                    event_record = EventRecord(city,testpoint_name, testscene, servicetype, totalcount, coveredcount,
+                                                       abnormaltype,moattempttime,mofilename)
+                                    record_list.append(event_record)
 
                         # 查找电信掉话详情表找到主叫文件名、主叫起呼时间
                         if  not droppedcount is None and  droppedcount > 0:
@@ -164,18 +162,14 @@ def extract_2018_records(fdpath):
                             testscene = sht_content[i][4]
                             totalcount = sht_content[i][12]
                             coveredcount = sht_content[i][11]
-                            dropped_detail_list = []
                             for k in range(5, drop_sht_max_row):
                                 mofilename = drop_event_content[k][5]
                                 moattempttime = drop_event_content[k][6]
                                 if testpoint_name in str(mofilename) and testscene in str(mofilename):
                                     abnormaltype = '掉话'
-                                    abnormal_event = EventRecord.AbnormalDetail(mofilename, moattempttime)
-                                    dropped_detail_list.append(abnormal_event)
-                                    if len(dropped_detail_list) >= droppedcount:break
-                            event_record = EventRecord(city,testpoint_name, testscene, servicetype, totalcount, coveredcount,
-                                                       abnormaltype, dropped_detail_list)
-                            record_list.append(event_record)
+                                    event_record = EventRecord(city,testpoint_name, testscene, servicetype, totalcount, coveredcount,
+                                                       abnormaltype,moattempttime,mofilename)
+                                    record_list.append(event_record)
 
             if servicetype == '': return
         except Exception as e:
@@ -236,12 +230,9 @@ def extract_2019_records(fdpath):
                                 moattempttime = block_event_content[j][3]
                                 if testpoint_name in str(mofilename) and testscene in str(mofilename):
                                     abnormaltype = '未接通'
-                                    abnormal_event = EventRecord.AbnormalDetail(mofilename, moattempttime)
-                                    blocked_detail_list.append(abnormal_event)
-                                    if len(blocked_detail_list) >= blockedcount: break
-                            event_record = EventRecord(city,testpoint_name, testscene, servicetype, totalcount, coveredcount,
-                                                       abnormaltype, blocked_detail_list)
-                            record_list.append(event_record)
+                                    event_record = EventRecord(city,testpoint_name, testscene, servicetype, totalcount, coveredcount,
+                                                       abnormaltype,moattempttime,mofilename)
+                                    record_list.append(event_record)
 
                         if not droppedcount is None and droppedcount > 0:
                             city = sht_content[i][2]
@@ -255,12 +246,9 @@ def extract_2019_records(fdpath):
                                 moattempttime = drop_event_content[k][3]
                                 if testpoint_name in str(mofilename) and testscene in str(mofilename):
                                     abnormaltype = '掉话'
-                                    abnormal_event = EventRecord.AbnormalDetail(mofilename, moattempttime)
-                                    dropped_detail_list.append(abnormal_event)
-                                    if len(dropped_detail_list) >= droppedcount:break
-                            event_record = EventRecord(city,testpoint_name, testscene, servicetype, totalcount, coveredcount,
-                                                       abnormaltype, dropped_detail_list)
-                            record_list.append(event_record)
+                                    event_record = EventRecord(city,testpoint_name, testscene, servicetype, totalcount, coveredcount,
+                                                       abnormaltype,moattempttime,mofilename)
+                                    record_list.append(event_record)
 
                 elif sht.name == sht_volte_name:
                     servicetype = 'volte'
@@ -287,12 +275,9 @@ def extract_2019_records(fdpath):
                                 moattempttime = block_event_content[j][4]
                                 if testpoint_name in str(mofilename) and testscene in str(mofilename):
                                     abnormaltype = '未接通'
-                                    abnormal_event = EventRecord.AbnormalDetail(mofilename, moattempttime)
-                                    blocked_detail_list.append(abnormal_event)
-                                    if len(blocked_detail_list) >= blockedcount:break
-                            event_record = EventRecord(city,testpoint_name, testscene, servicetype, totalcount, coveredcount,
-                                                       abnormaltype, blocked_detail_list)
-                            record_list.append(event_record)
+                                    event_record = EventRecord(city,testpoint_name, testscene, servicetype, totalcount, coveredcount,
+                                                       abnormaltype,moattempttime,mofilename)
+                                    record_list.append(event_record)
 
                         # 查找电信掉话详情表找到主叫文件名、主叫起呼时间
                         if  not droppedcount is None and  droppedcount > 0:
@@ -307,12 +292,9 @@ def extract_2019_records(fdpath):
                                 moattempttime = drop_event_content[k][4]
                                 if testpoint_name in str(mofilename) and testscene in str(mofilename):
                                     abnormaltype = '掉话'
-                                    abnormal_event = EventRecord.AbnormalDetail(mofilename, moattempttime)
-                                    dropped_detail_list.append(abnormal_event)
-                                    if len(dropped_detail_list) >= droppedcount:break
-                            event_record = EventRecord(city,testpoint_name, testscene, servicetype, totalcount, coveredcount,
-                                                       abnormaltype, dropped_detail_list)
-                            record_list.append(event_record)
+                                    event_record = EventRecord(city,testpoint_name, testscene, servicetype, totalcount, coveredcount,
+                                                       abnormaltype,moattempttime,mofilename)
+                                    record_list.append(event_record)
 
 
         except Exception as e:
@@ -331,7 +313,7 @@ def compare_records(aReacords,bRecords):
 
 class EventRecord():
     def __init__(self, city,testpoint_name, testscene, servicetype, totalcount, coveredcount, abnormaltype,
-                 abnormaldetaillist):
+                 mocallattempttime,mofilename):
         self.city = city #城市
         self.testpoint_name = testpoint_name  # 测试点名称
         self.testscene = testscene  # 测试场景类型（深度，浅度）
@@ -339,35 +321,37 @@ class EventRecord():
         self.totalcount = totalcount  # 采样点分母
         self.coveredcount = coveredcount  # 采样点分子
         self.abnormaltype = abnormaltype  # 异常类型（掉话、未接通）
-        self.abnormaldetaillist = abnormaldetaillist  # 异常详情列表
+        self.mocallattempttime = mocallattempttime #主叫起呼时间
+        self.mofilename = mofilename #主叫文件名
+        #self.abnormaldetaillist = abnormaldetaillist  # 异常详情列表
 
     def __eq__(self, other):
-        return self.city+self.testpoint_name+self.testscene+self.servicetype+self.abnormaltype == \
-               other.city+other.testpoint_name+other.testscene+other.servicetype+other.abnormaltype
+        return self.city+self.testpoint_name+self.testscene+self.servicetype+self.abnormaltype + str(round(self.mocallattempttime,6)) == \
+               other.city+other.testpoint_name+other.testscene+other.servicetype+other.abnormaltype + str(round(self.mocallattempttime,6))
 
     def __hash__(self):
-        return hash(self.city+self.testpoint_name+self.testscene+self.servicetype+self.abnormaltype)
+        return hash(self.city+self.testpoint_name+self.testscene+self.servicetype+self.abnormaltype + str(round(self.mocallattempttime,6)) )
 
     def __repr__(self):
-        outstr = "city:{}\ntestpoint_name:{}\ntestscene:{}\nservicetype:{}\ntotalcount:{}\ncoveredcount:{}\nabnormaltype:{}\nabnormaldetaillist:{}\n".format(
-            self.city,self.testpoint_name,self.testscene,self.servicetype,self.totalcount,self.coveredcount,self.abnormaltype,self.abnormaldetaillist)
+        fmt_mocallattempttime = floattimetostr(self.mocallattempttime)
+        outstr = "city:{}\ntestpoint_name:{}\ntestscene:{}\nservicetype:{}\ntotalcount:{}\ncoveredcount:{}\nabnormaltype:{}\nfmt_mocallattempttime:{}\nmofilename:{}\n".format(
+            self.city,self.testpoint_name,self.testscene,self.servicetype,self.totalcount,self.coveredcount,self.abnormaltype,fmt_mocallattempttime,self.mofilename)
         return outstr
 
-    class AbnormalDetail():
-        def __init__(self, mofilename, mocallattempttime):
-            self.mofilename = mofilename
-            self.mocallattempttime = mocallattempttime
-
-        def __eq__(self, other):
-            return round(self.mocallattempttime,6) == round(other.mocallattempttime,6)
-
-        def __hash__(self):
-            return hash(round(self.mocallattempttime,6))
-
-        def __repr__(self):
-            time_stamp = (self.mocallattempttime - 25569)
-            outstr = floattimetostr(self.mocallattempttime)
-            return outstr
+    # class AbnormalDetail():
+    #     def __init__(self, mofilename, mocallattempttime):
+    #         self.mofilename = mofilename
+    #         self.mocallattempttime = mocallattempttime
+    #
+    #     def __eq__(self, other):
+    #         return round(self.mocallattempttime,6) == round(other.mocallattempttime,6)
+    #
+    #     def __hash__(self):
+    #         return hash(round(self.mocallattempttime,6))
+    #
+    #     def __repr__(self):
+    #         outstr = floattimetostr(self.mocallattempttime)
+    #         return outstr
 
 
 def floattimetostr(floattime):
